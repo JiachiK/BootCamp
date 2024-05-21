aea1009a86b5a87bbb64123f5657aa5b9764de76
JiachiK <164652861+JiachiK@users.noreply.github.com>
2024-05-21 09:38:01 +0800

    Add files via upload

---
+++ UI.py
@@ -0,0 +1,6 @@
+import streamlit as st  
+
+st.set_page_config(page_title= "ChatBot" , page_icon = ":books:")
+st.title = (":books: AskNarelle")
+st.header("This is a header");
+st.markdown("Hello World :sunglasses:")
